24077090aab170c383021c6ff9d33b4e5ac853bc
Calvin <hello@calvin-tan.com>
2024-04-10 03:50:05 +0800

    Add fullscreen menu, background image animation when links are hovered

--- header.php
+++ header.php
@@ -18,9 +18,52 @@
 </head>
 <body <?php body_class(); ?>>
 
-<header role="banner">
-  <nav class="nav" role="navigation"></nav>
-</header>
+<!--
+  <header role="banner">
+    <nav class="nav" role="navigation"></nav>
+  </header>
+-->
+
+<section class="overlay">
+  <div class="overlay__bg">
+    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/bg-menu-1.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/bg-menu-2.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/bg-menu-3.jpg" alt="">
+  </div>
+  <div class="overlay__cover"></div>
+  <div class="overlay__line"></div>
+
+  <div class="overlay__text">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+
+          <ul class="overlay__menu">
+            <li><a href="#">Who We Are</a></li>
+            <li><a href="#">Investor Roadshow Videos</a></li>
+            <li><a href="#">Documentaries</a></li>
+          </ul>
+
+          <ul class="overlay__contact footer__contact">
+            <li>
+              <h3>Email</h3>
+              <a href="#">info@relay.video</a>
+            </li>
+            <li>
+              <h3>WeChat</h3>
+              <a href="#">XFSP1989</a>
+            </li>
+            <li>
+              <h3>WhatsApp</h3>
+              <a href="#">Abe</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <div class="menu">
   <div class="container">
@@ -30,7 +73,7 @@
       </a>
       <div class="menu__wrap d-flex align-items-center gap-5">
         <a class="menu__lang" href="#">中</a>
-        <a href="#">
+        <a class="hamburger" href="#">
           <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icon-menu.svg" alt="Menu icon">
         </a>
       </div>
--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,5 +1,36 @@
 import $ from "jquery";
 
 $(document).ready(() => {
-// Stuff goes here
+
+// Fullscreen menu
+$(".hamburger").on('click', function(e) {
+  e.preventDefault;
+  console.log('testing!');
+  $(".overlay").toggleClass('fade-in');
+});
+
+$(".overlay__menu li:nth-child(1) a").mouseover(function () {
+  $(".overlay__bg img:nth-child(1)").addClass("fade-in");
+});
+
+$(".overlay__menu li:nth-child(1) a").mouseout(function () {
+  $(".overlay__bg img:nth-child(1)").removeClass("fade-in");
+});
+
+$(".overlay__menu li:nth-child(2) a").mouseover(function () {
+  $(".overlay__bg img:nth-child(2)").addClass("fade-in");
+});
+
+$(".overlay__menu li:nth-child(2) a").mouseout(function () {
+  $(".overlay__bg img:nth-child(2)").removeClass("fade-in");
+});
+
+$(".overlay__menu li:nth-child(3) a").mouseover(function () {
+  $(".overlay__bg img:nth-child(3)").addClass("fade-in");
+});
+
+$(".overlay__menu li:nth-child(3) a").mouseout(function () {
+  $(".overlay__bg img:nth-child(3)").removeClass("fade-in");
+});
+
 });
