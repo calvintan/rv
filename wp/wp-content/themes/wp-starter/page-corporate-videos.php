b8274b18e1d3cb1c0ec33bd4d23cb44c9bbdb58c
Jack <jacklee@Jacks-MacBook-Air.local>
2024-04-10 15:25:47 +0800

    common varirables added and corporate page updated

--- wp/wp-content/themes/wp-starter/page-corporate-videos.php
+++ wp/wp-content/themes/wp-starter/page-corporate-videos.php
@@ -1,24 +1,41 @@
 <?php get_header(); ?>
 
-<section class="hero">
-  <div class="hero__text">
+<section class="corporate">
+  <div class="corporate__text">
     <div class="container">
-      <div class="row">
-        <div class="col-10 col-md-8 col-lg-6">
-          <h1 class="page__title">
-            <?php single_post_title(); ?>
-          </h1>
-      </div>
-    </div>
+      <h1 class="corporate__title" >
+        <?php single_post_title(); ?>
+      </h1>
   </div>
-  <div class="hero__overlay">
-
+  <div class="corporate__overlay">
   </div>
-  <div class="hero__video">
-    <video autoplay="" loop="" muted="">
-      <source src="//localhost:3000/wp-content/themes/wp-starter/dist/assets/videos/hero.webm" type="video/webm">
+  <div class="corporate__video">
+    <video autoplay muted loop>
+      <source src="//localhost/wp-content/themes/wp-starter/dist/assets/videos/hero.mp4" type="video/mp4">
+      <source src="//localhost/wp-content/themes/wp-starter/dist/assets/videos/hero.webm" type="video/webm">
+      Your browser does not support HTML5 video.
     </video>
   </div>
 </section>
+
+<main role="main">
+  <section class="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-lg-4 about__heading">
+          <h1>
+            We are the market leader for investor roadshow videos.
+          </h1>
+        </div>
+        <div class="col-sm-12 col-lg-8 about__text">
+          <p>
+            We have proven success with making investor roadshow videos for China’s biggest IPOs: TME, Pinduoduo, Mogujie, Sogou, J&T, Meituan, 360Finance, Pop Mart, Leap Motor and many more companies. 
+            Our clients have raised over 8 billion USD, and Relay Video helped them do that through making videos to explain their company and products in roadshow presentations.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <?php get_footer(); ?>
