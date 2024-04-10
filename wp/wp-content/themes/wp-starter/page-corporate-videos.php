00b6a9588da6c7013cb30245ebd60d9806e55742
Jack <jacklee@Jacks-MacBook-Air.local>
2024-04-10 08:14:30 +0800

    updates on corporate page

--- wp/wp-content/themes/wp-starter/page-corporate-videos.php
+++ wp/wp-content/themes/wp-starter/page-corporate-videos.php
@@ -20,21 +20,5 @@
     </video>
   </div>
 </section>
-<!-- <main role="main">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <header class="page__header">
-          <h1 class="page__title">
-            <?php single_post_title(); ?>
-          </h1>
-        </header>
-      </div>
-    </div>
-    <div class="row">
-      <?php get_template_part('loop', 'index'); ?>
-    </div>
-  </div>
-</main> -->
 
 <?php get_footer(); ?>
