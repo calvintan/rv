09679a44303935ef5514044b75e611e71a22e461
Calvin <hello@calvin-tan.com>
2024-04-11 16:40:55 +0800

    Merge corporate page, update code to link with CMS

--- page-corporate-videos.php
+++ page-corporate-videos.php
@@ -1,25 +1,64 @@
 <?php get_header(); ?>
 
+<section class="corporate">
+  <div class="corporate__text">
+    <div class="container">
+      <h1 class="corporate__title"><?php single_post_title(); ?></h1>
+    </div>
+  </div>
+  <div class="corporate__overlay"></div>
+  <div class="corporate__video">
+    <video autoplay muted loop>
+      <source src="<?php echo get_template_directory_uri(); ?>/dist/assets/videos/corporate-short-720.mp4" type="video/mp4">
+      <source src="<?php echo get_template_directory_uri(); ?>/dist/assets/videos/corporate-short-720.webm" type="video/webm">
+      Your browser does not support HTML5 video.
+    </video>
+  </div>
+</section>
+
 <main role="main">
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <p>This is Corporate Videos page</p>
+  <section class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-4">
+          <h1>
+            We are the market leader for investor roadshow videos.
+          </h1>
+        </div>
+        <div class="col-12 col-lg-8">
+          <p>We have proven success with making investor roadshow videos for China's biggest IPOs: TME, Pinduoduo, Mogujie, Sogou, J&T, Meituan, 360Finance, Pop Mart, Leap Motor and many more companies.</p>
+
+          <p>Our clients have raised over 8 billion USD, and Relay Video helped them do that through making videos to explain their company and products in roadshow presentations.</p>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="example">
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+  </section>
+
+  <section class="video">
+    <div class="container-fluid">
+      <div class="row">
         <?php
-          $press_query = array(
+          $corporate_query = array(
             'post_type' => 'video',
             'posts_per_page' => -1,
             'post_status' => 'publish',
             'meta_key' => 'type',
             'meta_value' => 'corporate'
-
           );
-          $press = new WP_Query( $press_query );
+          $corporate = new WP_Query( $corporate_query );
         ?>
-        <?php if ($press->have_posts()) : ?>
-          <?php while ( $press->have_posts() ) : $press->the_post(); ?>
+        <?php if ($corporate->have_posts()) : $item = 0; ?>
+          <?php while ( $corporate->have_posts() ) : $corporate->the_post(); $item++; ?>
             <div class="col-md-6">
-              <a href="#"><?php the_title(); ?></a>
+              <a id="openPopup<?php echo $item; ?>"><?php the_title(); ?></a>
               <?php if ( has_post_thumbnail() ) {
                 the_post_thumbnail();
               } ?>
@@ -28,10 +67,46 @@
           <?php wp_reset_postdata(); ?>
         <?php endif; ?>
 
-        <?php get_template_part('loop', 'page'); ?>
+        <!-- Loop through the videos and create a video card for each video -->
+        <!-- The variables can be changed to the video data from the database -->
+        <?php
+          for ($i = 1; $i <= 6; $i++) {
+              echo '<div 
+              class="video__container
+              col-sm-12 
+              col-md-6 
+              col-lg-6" 
+              id="openPopup'.$i.'">
+              <div class="video__card card-body">
+              <video id="video'.$i.'" 
+              class="video__video" 
+              poster="//localhost/relay/wp-content/themes/wp-starter/dist/assets/images/corporate-'.$i.'.jpg">
+              <source src="//localhost/relay/wp-content/themes/wp-starter/dist/assets/videos/hero.webm" type="video/webm"
+              </video></div></div>';
+          }
+        ?>
+
+
       </div>
     </div>
-  </div>
+    </div>
+  </section>
+
+  <!-- Loop through the videos and create a modal for each video -->
+  <!-- The variables can be changed to the video data from the database -->
+  <?php
+    for ($i = 1; $i <= 6; $i++) {
+        echo '<div id="videoPopup'.$i.'" class="modal">
+                <div class="modal__content">
+                  <span class="modal__close'.$i.'">&times;</span>
+                  <video id="video'.$i.'" controls poster="//localhost/relay/wp-content/themes/wp-starter/dist/assets/images/video-thumb-1.jpg">
+                    <source src="//localhost/relay/wp-content/themes/wp-starter/dist/assets/videos/hero.webm" type="video/webm">
+                  </video>
+                </div>
+              </div>';
+    }
+  ?>
+
 </main>
 
 <?php get_footer(); ?>
--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,36 +1,64 @@
 import $ from "jquery";
 
 $(document).ready(() => {
+  // Fullscreen menu
+  $(".hamburger").on('click', function(e) {
+    e.preventDefault;
+    console.log('testing!');
+    $(".overlay").toggleClass('fade-in');
+  });
 
-// Fullscreen menu
-$(".hamburger").on('click', function(e) {
-  e.preventDefault;
-  console.log('testing!');
-  $(".overlay").toggleClass('fade-in');
+  $(".overlay__menu li:nth-child(1) a").mouseover(function () {
+    $(".overlay__bg img:nth-child(1)").addClass("fade-in");
+  });
+
+  $(".overlay__menu li:nth-child(1) a").mouseout(function () {
+    $(".overlay__bg img:nth-child(1)").removeClass("fade-in");
+  });
+
+  $(".overlay__menu li:nth-child(2) a").mouseover(function () {
+    $(".overlay__bg img:nth-child(2)").addClass("fade-in");
+  });
+
+  $(".overlay__menu li:nth-child(2) a").mouseout(function () {
+    $(".overlay__bg img:nth-child(2)").removeClass("fade-in");
+  });
+
+  $(".overlay__menu li:nth-child(3) a").mouseover(function () {
+    $(".overlay__bg img:nth-child(3)").addClass("fade-in");
+  });
+
+  $(".overlay__menu li:nth-child(3) a").mouseout(function () {
+    $(".overlay__bg img:nth-child(3)").removeClass("fade-in");
+  });
 });
 
-$(".overlay__menu li:nth-child(1) a").mouseover(function () {
-  $(".overlay__bg img:nth-child(1)").addClass("fade-in");
-});
+// Video Modal
+class VideoModal {
+  constructor(modalId, openButtonId, closeButtonClass) {
+    this.modal = document.getElementById(modalId);
+    this.openButton = document.getElementById(openButtonId);
+    this.closeButton = document.getElementsByClassName(closeButtonClass)[0];
 
-$(".overlay__menu li:nth-child(1) a").mouseout(function () {
-  $(".overlay__bg img:nth-child(1)").removeClass("fade-in");
-});
+    this.openButton.onclick = () => this.open();
+    this.closeButton.onclick = () => this.close();
+    window.onclick = (event) => {
+      if (event.target == this.modal) {
+        this.close();
+      }
+    };
+  }
 
-$(".overlay__menu li:nth-child(2) a").mouseover(function () {
-  $(".overlay__bg img:nth-child(2)").addClass("fade-in");
-});
+  open() {
+    this.modal.style.display = "block";
+  }
 
-$(".overlay__menu li:nth-child(2) a").mouseout(function () {
-  $(".overlay__bg img:nth-child(2)").removeClass("fade-in");
-});
+  close() {
+    this.modal.style.display = "none";
+  }
+}
 
-$(".overlay__menu li:nth-child(3) a").mouseover(function () {
-  $(".overlay__bg img:nth-child(3)").addClass("fade-in");
-});
-
-$(".overlay__menu li:nth-child(3) a").mouseout(function () {
-  $(".overlay__bg img:nth-child(3)").removeClass("fade-in");
-});
-
-});
+// Usage
+for (let i = 1; i <= 6; i++) {
+  new VideoModal("videoPopup" + i, "openPopup" + i, "modal__close" + i);
+}
