5784d30d8dee36c8b21d35bf110847bc2231ca22
Jack <jacklee@Jacks-MacBook-Air.local>
2024-04-16 00:38:53 +0800

    wechat qr code added to footer and menu

--- footer.php
+++ footer.php
@@ -10,11 +10,11 @@
     </li>
     <li>
       <h3>WeChat</h3>
-      <a>XFSP1989</a>
+      <a data-bs-toggle="modal" data-bs-target="#wechat">XFSP1989 <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icon-external.svg" alt="open in pop-up"></a>
     </li>
     <li>
       <h3>WhatsApp</h3>
-      <a data-bs-toggle="modal" data-bs-target="#exampleModal">Abe <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icon-external.svg" alt="open in pop-up"></a>
+      <a data-bs-toggle="modal" data-bs-target="#whatsapp">Abe <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icon-external.svg" alt="open in pop-up"></a>
     </li>
   </ul>
   <div class="footer__bottom">
@@ -26,7 +26,22 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="wechat" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Add us on WeChat</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/wechat.jpg" alt="QR code">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="whatsapp" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-sm">
     <div class="modal-content">
       <div class="modal-header">
